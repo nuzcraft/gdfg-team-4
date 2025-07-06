535f3d82cbc9c029850f8d73eca321286fdb975b
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2025-07-06 11:35:31 -0400

    Add a Settings Button and Scene with a Music Checkbox placeholder

---
+++ scenes/settings.gd
@@ -0,0 +1,19 @@
+extends Control
+@onready var music_check: CheckBox = $MusicCheckBox
+@onready var back_btn:     Button   = $HBoxContainer/BackButton
+@onready var save_btn:     Button   = $HBoxContainer/SaveButton
+
+func _ready() -> void:
+  # Show the current value when we arrive
+  music_check.button_pressed = SettingsStore.enable_music
+
+  back_btn.pressed.connect(_go_back)
+  save_btn.pressed.connect(_save_and_back)
+
+func _go_back() -> void:
+  get_tree().change_scene_to_file("res://scenes/title.tscn")
+
+func _save_and_back() -> void:
+  SettingsStore.enable_music = music_check.button_pressed
+  # do any extra work here (write to disk, emit a signal, etc.)
+  get_tree().change_scene_to_file("res://scenes/title.tscn")
--- scenes/title.gd
+++ scenes/title.gd
@@ -9,3 +9,6 @@ func _ready() -> void:
 
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/prototype_01.tscn")
+
+func _on_settings_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/settings.tscn")
