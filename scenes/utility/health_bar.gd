fe6ca72a81752a164b8d2e5461134b5a11c8b44d
t-amitai <tamitai147@gmail.com>
2025-07-08 16:36:42 -0600

    adds health_bar script to update shader on player_health/player_armor chage

---
+++ scenes/utility/health_bar.gd
@@ -0,0 +1,27 @@
+extends Control
+
+@onready var health_bar = $Health
+@onready var armor_bar = $Armor
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+var last_health: int = Globals.player_health
+var last_armor: int = Globals.player_armor
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	var curr_health = Globals.player_health
+	var curr_armor = Globals.player_armor
+	
+	if last_health != curr_health:
+		health_bar.material.set_shader_parameter('health', curr_health / 100)
+		last_health = curr_health
+	if last_armor != curr_armor:
+		armor_bar.material.set_shader_parameter('armor', curr_armor / 100)
+		last_armor = curr_armor
+		
+	# add code to handle rendering extra health bars
+	# if Globals.player_max_health > 100
