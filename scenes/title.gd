caa757b8930fcf6ff79681a55a70b50c34dccbf6
Tamir Amitai <72648573+t-amitai@users.noreply.github.com>
2025-07-10 13:27:54 -0600

    Level system (#37)
    * fixes GUT unfreed children issue
    * moves HUD from game to hero
    * deletes game scene and script
    * adds levels/ with level1,2,3 scenes and level.gs that copies game.gd
    * in title.gd switches change to game scene to level1.tscn
    * updates smoke/test_game

--- scenes/title.gd
+++ scenes/title.gd
@@ -24,7 +24,7 @@ func _on_play_button_pressed():
 		Music.stop()
 
 	# change to the game scene
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	get_tree().change_scene_to_file("res://scenes/levels/level1.tscn")
 
 
 func _on_settings_button_pressed():
--- test/smoke/test_game.gd
+++ test/smoke/test_game.gd
@@ -1,22 +1,53 @@
 extends GutTest
 
-var game_scene := preload('res://scenes/game.tscn')
-var game
+var level1_scene := preload('res://scenes/levels/level1.tscn')
+var level2_scene := preload('res://scenes/levels/level2.tscn')
+var level3_scene := preload('res://scenes/levels/level3.tscn')
 
-func before_each():
-	game = game_scene.instantiate()
-	add_child_autoqfree(game)
+func test_level1_scene_loads():
+	var level = level1_scene.instantiate()
+	add_child_autofree(level)
 	await get_tree().process_frame
-
-func test_game_scene_loads():
-	# has game
-	assert_true(game is Node, "Game scene failed to load")
+	# has level
+	assert_true(level is Node, "level scene failed to load")
 	# has hero
-	var hero = game.get_node('Hero')
+	var hero = level.get_node('Hero')
 	assert_true(hero.get_scene_file_path() == 'res://scenes/hero.tscn')
 	assert_true(hero is Hero, 'hero is not of class Hero')
 	# has ground and deocration layers
-	var ground = game.get_node('TileMapLayers/GroundLayer')
-	var decoration = game.get_node('TileMapLayers/DecorationLayer')
+	var ground = level.get_node('TileMapLayers/GroundLayer')
+	var decoration = level.get_node('TileMapLayers/DecorationLayer')
+	assert_true(ground is TileMapLayer, 'Ground layer failed to load')
+	assert_true(decoration is TileMapLayer, 'Decoration layer failed to load')
+
+func test_level2_scene_loads():
+	var level = level2_scene.instantiate()
+	add_child_autofree(level)
+	await get_tree().process_frame
+	# has level
+	assert_true(level is Node, "level scene failed to load")
+	# has hero
+	var hero = level.get_node('Hero')
+	assert_true(hero.get_scene_file_path() == 'res://scenes/hero.tscn')
+	assert_true(hero is Hero, 'hero is not of class Hero')
+	# has ground and deocration layers
+	var ground = level.get_node('TileMapLayers/GroundLayer')
+	var decoration = level.get_node('TileMapLayers/DecorationLayer')
+	assert_true(ground is TileMapLayer, 'Ground layer failed to load')
+	assert_true(decoration is TileMapLayer, 'Decoration layer failed to load')
+
+func test_level3_scene_loads():
+	var level = level3_scene.instantiate()
+	add_child_autofree(level)
+	await get_tree().process_frame
+	# has level
+	assert_true(level is Node, "level scene failed to load")
+	# has hero
+	var hero = level.get_node('Hero')
+	assert_true(hero.get_scene_file_path() == 'res://scenes/hero.tscn')
+	assert_true(hero is Hero, 'hero is not of class Hero')
+	# has ground and deocration layers
+	var ground = level.get_node('TileMapLayers/GroundLayer')
+	var decoration = level.get_node('TileMapLayers/DecorationLayer')
 	assert_true(ground is TileMapLayer, 'Ground layer failed to load')
 	assert_true(decoration is TileMapLayer, 'Decoration layer failed to load')
