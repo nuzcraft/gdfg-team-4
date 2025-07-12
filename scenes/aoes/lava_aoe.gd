c88605a9c9853a715be39980924c32913be82d94
bleakgh <andrewemoorehead@gmail.com>
2025-07-12 02:08:41 -0700

    added initial lava pool damage code. will update with comprehensive code later.# Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.## On branch develop# Your branch is up to date with 'origin/develop'.## Changes to be committed:#	modified:   scenes/aoes/lava_aoe.gd#	modified:   scenes/aoes/lava_aoe.tscn#	modified:   scenes/hero.gd## Untracked files:#	gdfg-team-4/#	tash#

--- scenes/aoes/lava_aoe.gd
+++ scenes/aoes/lava_aoe.gd
@@ -1,8 +1,19 @@
 extends Area2D
 
 
+enum burn_state{
+	Start,
+	Hold,
+	End
+}
+
+func _ready():
+	pass
+
 func _on_body_entered(body):
 	if body.has_method("burn"):
-		body.burn()
-		await get_tree().create_timer(0.5).timeout
-		body.burn()
+		body.burn(burn_state.Start)
+
+func _on_body_exited(body):
+	if body.has_method("burn"):
+		body.burn(burn_state.End)
--- scenes/hero.gd
+++ scenes/hero.gd
@@ -3,7 +3,7 @@ class_name Hero
 
 var can_shoot: bool = true
 var can_melee: bool = true
-var currently_in_lava: bool = false
+var alive: bool = true
 
 
 @export var max_speed: int =500
@@ -36,9 +36,47 @@ func _process(_delta):
 	#Melee attack input
 	#if Input.is_action_pressed("secondaryAction") and can_melee:
 	#	pass
+func die():
+	if self.alive:
+		print("Player died.")
+		self.alive=false
 
-func burn():
-	Globals.player_health -= 5
-	currently_in_lava = true
-	$Label.text = "Burning"
-	
+func take_damage(n: int):
+	if Globals.player_health<=n:
+		self.die()
+	else:
+		Globals.player_health -= n
+		print("Player has "+str(Globals.player_health)+" health.")
+
+var lava_pools: int = 0
+var burn_count: int = 0
+enum burn_state{
+	Start,
+	Hold,
+	End
+}
+func burn(input:int):
+	if(input==burn_state.Start&&self.lava_pools<=0):
+		$Label.text = "Burning"
+		self.burn_count=0
+		self.lava_pools+=1
+		print("player is in "+str(self.lava_pools)+" lava pools")
+		await get_tree().create_timer(0.25).timeout
+		self.burn(burn_state.Hold)
+	if(input==burn_state.Start&&self.lava_pools>=1):
+		self.lava_pools+=1
+		print("player is in "+str(self.lava_pools)+" lava pools")
+	if(input==burn_state.Hold&&self.lava_pools>=1):
+		self.take_damage(1)
+		await get_tree().create_timer(0.25).timeout
+		self.burn(burn_state.Hold)
+	if(input==burn_state.Hold&&self.lava_pools<=0&&burn_count>=1):
+		self.take_damage(1)
+		self.burn_count-=1
+		await get_tree().create_timer(0.25).timeout
+		self.burn(burn_state.Hold)
+	if(input==burn_state.End):
+		self.lava_pools-=1
+		print("player is in "+str(self.lava_pools)+" lava pools")
+		if lava_pools<=0:
+			burn_count=10
