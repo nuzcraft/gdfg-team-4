24186dc6e19cd1f3fb32f5931da1d2924dbe8ee6
t-amitai <tamitai147@gmail.com>
2025-07-12 18:29:16 -0600

    refactors projectile.weapon property to use WeaponData class

--- scenes/projectile.gd
+++ scenes/projectile.gd
@@ -1,7 +1,7 @@
 extends Area2D
 class_name Projectile
 
-var weapon: Weapon
+var weapon: WeaponData
 var ttl := 0.0
 var direction: Vector2
 
@@ -12,17 +12,20 @@ func _ready():
 	rotate(direction.angle())
 	
 	ttl = weapon.total_ttl
-	$Sprite.texture = weapon.get_node("ProjectileType/Sprite2D").texture
-	$CollisionShape.shape = weapon.get_node("ProjectileType/CollisionShape2D").shape
+	$Sprite.texture = weapon.projectile_texture
+	$CollisionShape.shape = weapon.projectile_shape
 	
 func _physics_process(delta):
-	ttl = ttl - delta
-	if ttl <= 0.0 or !is_inside_tree() or !weapon.is_inside_tree():
+	if !is_inside_tree():
 		queue_free()
 	else:
-		var speed_factor := weapon.speed_curve.sample_baked(ttl/weapon.total_ttl) * weapon.speed
-		position = position + speed_factor * delta * direction
-		$Sprite.modulate = Color(1,1,1,sqrt(ttl/weapon.total_ttl))
+		ttl = ttl - delta
+		if ttl <= 0.0:
+			queue_free()
+		else:
+			var speed_factor := weapon.speed_curve.sample_baked(ttl/weapon.total_ttl) * weapon.speed
+			position = position + speed_factor * delta * direction
+			$Sprite.modulate = Color(1,1,1,sqrt(ttl/weapon.total_ttl))
 
 func _on_body_entered(body):
 	if body.has_method("hit"):
