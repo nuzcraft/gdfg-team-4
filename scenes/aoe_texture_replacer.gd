f7d4e104285d4b6d4c94b3ae6e0f57a37f7e89c6
nuzcraft <20135847+nuzcraft@users.noreply.github.com>
2025-07-16 23:07:17 -0500

    first draft aoe texturing shader

--- scenes/aoe_texture_replacer.gd
+++ scenes/aoe_texture_replacer.gd
@@ -13,12 +13,29 @@ func _process(delta: float) -> void:
 	var mat: ShaderMaterial = color_rect.material
 	mat.set_shader_parameter("source_tex", tex)
 	
+	for sub_node in canvas_group.get_children():
+		for sub_sub_node in sub_node.get_children():
+			if sub_sub_node is Timer:
+				var timer: Timer = sub_sub_node
+				if timer.time_left <= 0.1:
+					timer.get_parent().queue_free()
+	
 func add_sprite(node: Node2D) -> void:
+	var new_spr: Sprite2D
+	var new_timer: Timer
 	for sub_node in node.get_children():
 		if sub_node is Sprite2D:
-			var new_spr = sub_node.duplicate()
+			new_spr = sub_node.duplicate()
 			new_spr.global_position = node.global_position
+			new_spr.rotation = node.rotation
+			new_spr.scale = node.scale
 			var mat: CanvasItemMaterial = CanvasItemMaterial.new()
 			mat.blend_mode = mat.BLEND_MODE_ADD
 			new_spr.material = mat
 			canvas_group.add_child(new_spr)
+		elif sub_node is Timer:
+			new_timer = sub_node.duplicate()
+			new_timer.wait_time = sub_node.wait_time
+	new_spr.add_child(new_timer)
+	new_timer.start()
+	
