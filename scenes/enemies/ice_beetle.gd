c5630292e09aeed39c545a6fe742bf1da44cd454
t-amitai <tamitai147@gmail.com>
2025-07-19 11:22:51 -0600

    adds shooting logic to ice_beetle.gd

--- scenes/enemies/ice_beetle.gd
+++ scenes/enemies/ice_beetle.gd
@@ -1,6 +1,21 @@
 extends Enemy
 class_name IceBeetle
 
+var can_shoot: bool = true
+
+var weapon: Weapon
+var shot_timer: Timer
+
+# new states for ice beetle
+enum {
+	SHOOTING = 4
+}
+
+func _ready() -> void:
+	super._ready()
+	weapon = $Weapon
+	shot_timer = $ShotTimer
+
 func _on_attack_area_2d_body_entered(body):
 	if body is Hero:
 		switch_state(SHOOTING)
@@ -8,3 +23,25 @@ func _on_attack_area_2d_body_entered(body):
 func _on_attack_area_2d_body_exited(body):
 	if body is Hero:
 		switch_state(IDLE)
+
+func _process(delta):
+	match state:
+		SHOOTING:
+				if can_shoot:
+					target_pos = target.position
+					var _target_dir = (target_pos - position).normalized()
+					weapon.fire(_target_dir)
+					can_shoot = false
+					shot_timer.start()
+		_:
+			super._process(delta)
+
+func switch_state(state_enum) -> void:
+	match state_enum:
+		SHOOTING:
+			state = SHOOTING
+		_:
+			super.switch_state(state_enum)
+
+func _on_shot_timer_timeout():
+	can_shoot = true
