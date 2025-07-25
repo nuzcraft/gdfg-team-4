7f9a888d9d359841283e6a003d02d6b92d3debb3
t-amitai <tamitai147@gmail.com>
2025-07-25 09:07:39 -0600

    refactors _next_level

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -10,6 +10,13 @@ const CRYSTAL = preload("res://scenes/collectables/crystal.tscn")
 const SPAWNER = preload("res://scenes/enemies/spawner.tscn")
 const LEVEL_2: CompressedTexture2D = preload("res://assets/levels/level2.png")
 
+const LEVELS = [
+	"res://scenes/levels/level1.tscn",
+	"res://scenes/levels/level2.tscn"
+]
+
+@export var current_level = 0
+
 var num_enemies_spawned = 0
 var level_images = {
 	"Level2" : LEVEL_2
@@ -73,28 +80,12 @@ func _enemy_wave_cleared() -> bool:
 		return false
 
 func _next_level():
-	var scene_name: String = str(get_tree().current_scene.name)
-	var scene_file: String = str(get_tree().current_scene.scene_file_path)
-	var regex = RegEx.new()
-	regex.compile("Level(\\d+)")
-	var name_result = regex.search(scene_name)
-	regex.compile("res://scenes/levels/level(\\d+).tscn")
-	var file_result = regex.search(scene_file)
-	if name_result and file_result:
-		var num := int(name_result.get_string(1))
-		if num < MAX_LEVELS:
-			var file_num := int(file_result.get_string(1))
-			if num == file_num:
-				var next_level := "res://scenes/levels/level" + str(num + 1) + ".tscn"
-				print("Switching to: ", next_level)
-				call_deferred('_change_scene_safe', next_level)
-			else:
-				call_deferred('_change_scene_safe', 'res://scenes/utility/title.tscn')
-		else:
-			print("Game over")
-			call_deferred('_change_scene_safe', 'res://scenes/utility/title.tscn')
+	current_level += 1
+	if current_level >= LEVELS.size():
+		call_deferred('_change_scene_safe', 'res://scenes/utility/title.tscn')
 	else:
-		push_error("Level name didn't match expected pattern.")
+		var next_level = LEVELS[current_level]
+		call_deferred('_change_scene_safe', next_level)
 
 func _change_scene_safe(scene_path: String):
 	get_tree().change_scene_to_file(scene_path)
