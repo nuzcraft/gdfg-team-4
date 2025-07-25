927e6ca91044ce582d2725ec9564208a362e52b1
t-amitai <tamitai147@gmail.com>
2025-07-25 08:48:16 -0600

    anotha one

--- test/smoke/test_game.gd
+++ test/smoke/test_game.gd
@@ -4,7 +4,7 @@ var level1_scene := preload('res://scenes/levels/level1.tscn')
 
 func test_level1_scene_loads():
 	var level = level1_scene.instantiate()
-	get_tree().current_scene.name = level.name
+	get_tree().current_scene = level
 	add_child_autofree(level)
 	await get_tree().process_frame
 	# has level
@@ -14,4 +14,4 @@ func test_level1_scene_loads():
 	assert_true(hero.get_scene_file_path() == 'res://scenes/hero.tscn')
 	assert_true(hero is Hero, 'hero is not of class Hero')
 	# cleanup
-	get_tree().current_scene.name = ''
+	get_tree().current_scene = null
