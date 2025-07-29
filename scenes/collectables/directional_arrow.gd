2d623a7ebafe44bf9cee9bd3a359f3d807645db6
BardicSoulkeeper <164677640+BardicSoulkeeper@users.noreply.github.com>
2025-07-28 21:25:51 -0600

    I added an arrow that points to the crystals
    I created a global group called "hero" to reference the player character. I added directional arrows as children of the crystal scene. Each crystal now has an arrow sprite2D that follows the player and points at it's crystal until the crystal is collected.

---
+++ scenes/collectables/directional_arrow.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+var hero_node: CharacterBody2D = null
+@export var target: Node2D
+var distance: float
+
+func _ready():
+	hero_node = get_tree().get_first_node_in_group("Hero")
+
+func _process(_delta):
+	if hero_node:
+		$".".global_position = hero_node.global_position
+		$Sprite2D.global_position = $".".global_position
+		#$Sprite2D.global_position = hero_node.global_position
+	if target != null:
+		$Sprite2D.look_at(target.global_position)
+		distance = global_position.distance_to(target.global_position)
+	print(distance)
+	if distance < 590:
+		$Sprite2D.visible = false
+	else: 
+		$Sprite2D.visible = true
+	
+	#keep the position on the player
+	#keep the direction to the gem & turn invisible if too close to gem
