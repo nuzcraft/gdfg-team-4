9eec4d8d2199a2482ec209e1668e5196bf7c5bc4
t-amitai <tamitai147@gmail.com>
2025-08-02 17:16:42 -0600

    adds portal scene

---
+++ scenes/levels/portal.gd
@@ -0,0 +1,21 @@
+extends Node2D
+class_name Portal
+
+signal portal
+
+func _ready():
+	$DirectionalArrow.get_node('Sprite2D').self_modulate = Color(0,0,1)
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is Hero:
+		print('start')
+		$Timer.start()
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body is Hero:
+		print('stop')
+		$Timer.stop()
+
+func _on_timer_timeout() -> void:
+	print('timeout')
+	emit_signal('portal')
