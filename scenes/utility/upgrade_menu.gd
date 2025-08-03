a324bd0b9c99a61caf9c34585715970e281a5d2c
t-amitai <tamitai147@gmail.com>
2025-08-03 07:54:23 -0600

    starts on upgrade menu script

---
+++ scenes/utility/upgrade_menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+@onready var Crystals := $TextPopUp/VBoxContainer/Info/GemsCount/Value
+@onready var Level := $TextPopUp/VBoxContainer/Info/CurrentLevel/Value
+
+func _ready():
+	Crystals.text = str(Globals.crystals_collected)
+
+
+
+func _on_title_screen_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/utility/title.tscn")
+
+
+func _on_restart_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/levels/level1.tscn")
